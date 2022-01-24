eef6faca6dbfa0f7d5099e815f930ca321a9497d
MichaelAldeni <98100988+MichaelAldeni@users.noreply.github.com>
2022-01-24 14:48:00 +0100

    Add files via upload

--- Biblioteca.h
+++ Biblioteca.h
@@ -2,7 +2,10 @@
 #include <stdlib.h>
 #include <string>
 #include <cstring>
+#include <vector>
 #include <fstream>
+
+using namespace std;
 
 const char DIPUSER[11] = "DIPENDENTE";
 const char DIPPASS[14] = "DIPENDENTE123";
