2a347773ddd11ea175e61a940abb4100e98e4e66
villa0990 <98102391+villa0990@users.noreply.github.com>
2022-01-27 16:02:52 +0100

    Add files via upload

--- Clienti.h
+++ Clienti.h
@@ -1,5 +1,5 @@
-/// <seealso cref="Biblioteca.h"/>
 #include "Biblioteca.h"
+
 using namespace std;
 
 
@@ -7,91 +7,98 @@ class Clienti {
 	/// <summary>Classe clienti</summary> 
 	/// <remarks>
 	/// <list type="bullet">
-	/// <item>Questa classe conterr√† tutti i clienti della libreria.</item> 
+	/// <item>Questa classe conterr‡ tutti i clienti della libreria</item> 
 	/// </remarks>
 
-	/// <summary>Queste sono le variabili utilizzate per l'implementazione della lista.</summary>
-
-	/// <param name="nome">Stringa per gestire il nome dell'utente.</param>
+	/// <summary>Queste sono le variabili utilizzate per l'implementazione della lista</summary>
+	/// <param name="nome">Stringa per gestire il nome dell'utente</param>
 	string nome;
 
-	/// <param name="cognome">Stringa per gestire il cognome dell'utente.</param>
+	/// <param name="cognome">Stringa per gestire il cognome dell'utente</param>
 	string cognome;
 
-	/// <param name="user">Variabile che gestisce la credenziale di accesso per il login nel programma dell'utente.</param>
+	/// <param name="user">Variabile che gestisce la credenziale di accesso per il login nel programma dell'utente</param>
 	string user;
 
-	/// <param name="password">Password utilizzata per il login nel programma da parte dell'utente.</param>
+	/// <param name="password">Password utilizzata per il login nel programma da parte dell'utente</param>
 	string password;
 
-	/// <param name="codice_fiscale">Codice fiscale, utilizzato per identificare il cliente.</param>
+	/// <param name="codice_fiscale">Codice fiscale, utilizzato per identificare il cliente </param>
 	string codice_fiscale;
 
-	/// <param name="numero_telefono">Variabile che gestisce i numeri di telefono dei vari clienti, 1 per ogni cliente.</param>
-	long int numero_telefono;
+	/// <param name="numero_telefono">Variabile che gestisce i numeri di telefono dei vari clienti, 1 per ogni cliente</param>
+	int numero_telefono;
 
-	/// <param name="n_prestito">Variabile riguardante la gestione dei libri in prestito per ogni cliente.</param>
+	/// <param name="n_prestito">Variabile riguardante la gestione dei libri in prestito per ogni cliente</param>
 	int n_prestito;
 
-
 public:
+	///<summary>Funzioni pubbliche della classe dei Clienti</summary>
 
-	/// <summary>Funzioni pubbliche della classe dei Clienti.</summary>
+	///<param name="Clienti">Costruttore per la classe Clienti, la inizializza</param> 
+	Clienti(string n, string c, string cod_fisc, string us_name, string pass, int num);
 
+	///<param name="setNome">Funzione di assegnazione del nome</param>
+	void setNome(string n) { nome = n; }
 
-	/// <item>Costruttore per la classe Clienti, la inizializza.</item> 
-	Clienti(string n, string c, string us_name, string pass, long int num = 0);
+	///<param name="setCognome">Funzione di assegnazione per il cognome</param>
+	void setCognome(string c) { cognome = c; }
 
-	/// <item>Funzione di assegnazione del nome.</item>
-	void setNome(string n) { nome = n; };
+	///<param name="setUser">Funzione di assegnazione per il nome utente scelto dal cliente</param>
+	void setUser(string us) { user = us; }
 
-	/// <item>Funzione di assegnazione per il cognome.</item>
-	void setCognome(string c) { cognome = c; };
+	///<param name="setPassword">Funzione di assegnazione della password scelta dal cliente</param>
+	void setPassword(string pass) { password = pass; }
 
-	/// <item>Funzione di assegnazione per il nome utente scelto dal cliente.</item>
-	void setUser(string us) { user = us; };
+	///<param name="setCod_fiscale">Funzione di assegnazione codice fiscale del cliente</param>
+	void setCod_fiscale(string cod) { codice_fiscale = cod; }
 
-	/// <item>Funzione di assegnazione della password scelta dal cliente.</item>
-	void setPassword(string pass) { password = pass; };
+	///<param name="setNumero">Funzione di assegnazione del numero di telefono del cliente</param>
+	void setNumero(int num) { numero_telefono = num; }
 
-	/// <item>Funzione di assegnazione codice fiscale del cliente.</item>
-	void setCod_fiscale(string cod) { codice_fiscale = cod; };
+	///<param name="getNome">Funzione di restituzione del nome del cliente</param>
+	string getNome()const { return nome; }
+	///<returns>Restituisce il nome del cliente</returns>
 
-	/// <item>Funzione di assegnazione del numero di telefono del cliente.</item>
-	void setNumero(long int num) { numero_telefono = num; };
+	///<param name="getCognome">Funzione di restituzuione del cognome del cliente</param>
+	string getCognome()const { return cognome; }
+	///<returns>Restituisce il cognome del cliente</returns>
 
-	/// <item>Funzione di restituzione del nome del cliente.</item>
-	/// <returns>Restituisce il nome del cliente.</returns>
-	/// <seealso cref="codice_fiscale"/>
-	string getNome()const { return nome; };
+	///<param name="getUser">Funzione di restituzuione del nome utetnte del cliente</param>
+	string getUser()const { return user; }
 
+	///<param name="getPassword">Funzione di restituzuione della password del cliente, costante in modo da non essre modificata</param>
+	string getPassword()const { return password; }
+	///<returns>Restituisce la password del cliente</returns>
 
-	/// <item>Funzione di restituzuione del cognome del cliente.</item>
-	/// <returns>Restituisce il cognome del cliente.</returns>
-	/// <seealso cref="cognome"/>
-	string getCognome()const { return cognome; };
+	///<param name="getCod_fiscale">Funzione di restituzuione della codice fiscale del cliente</param>
+	string getCod_fiscale()const { return codice_fiscale; }
+	///<returns>Restituisce il codice fiscale del cliente</returns>
 
-	/// <item>Funzione di restituzuione del nome utetnte del cliente.</item>
-	/// <returns>Restituisce il nome utente del cliente.</returns>
-	/// <seealso cref="user"/>
-	string getUser()const { return user; };
-
-	/// <item>Funzione di restituzuione della password del cliente, costante in modo da non essre modificata.</item>
-	/// <returns>Restituisce la password del cliente.</returns>
-	/// <seealso cref="password"/>
-	string getPassword()const { return password; };
-
-
-	/// <item>Funzione di restituzuione della codice fiscale del cliente.</item>
-	/// <returns>Restituisce il codice fiscale del cliente.</returns>
-	/// <seealso cref="codice_fiscale"/>
-	string getCod_fiscale()const { return codice_fiscale; };
+	///<param name="getNumero">Funzione di restituzuione della numero del cliente</param>
+	long int getNumero()const { return numero_telefono; }
+	///<returns>Restituisce il numero di telefono del cliente</returns>
 
 
-	/// <item>Funzione di restituzuione della numero del cliente.</item>
-	/// <returns>Restituisce il numero di telefono del cliente.</returns>
-	/// <seealso cref="numero_telefono"/>
-	long int getNumero()const { return numero_telefono; };
+	friend void registrazione(vector <Clienti>& vect);
+	//funzione che permette di effettuare la registrazione di un nuovo cliente, i dati dei clienti sono memorizzati in un vettore
+	friend void elimina_Cliente(Clienti x, vector <Clienti>& vect);
+	//funzione che permette di eliminare un cliente dal vettore
+	friend void stampa_vettore(vector <Clienti>& vect);
+	//stampa delle informazione di tutti i clienti registrati 
+	friend void stampa_Cliente(string user, vector <Clienti>& vect);
+	//stampa delle informazioni di un determinato cliente
+
+	friend bool is_on(string str, vector <Clienti>& vect);
+	//funzione ausiliaria, controlla se la stringa str Ë presente nel vettore vect
+	friend bool ceck_pass(string str);
+	//funzione ausiliaria, controlla la validit‡ della password
+
+
+
+	// OVERLOAD DEGLI OPERATORI 
+	friend ostream &operator<<(ostream &os, Clienti x);
+
 
 
 };
