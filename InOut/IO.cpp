688037346d3b79dc9220f8cc4d423e9e9a311918
MichaelAldeni <98100988+MichaelAldeni@users.noreply.github.com>
2022-02-15 09:55:13 +0100

    Add files via upload

---
+++ InOut/IO.cpp
@@ -0,0 +1,151 @@
+#include"IO.h"
+
+
+
+vector<Libri> carica_libri() {
+	vector<Libri> v;
+
+	ifstream ifs;
+	string filename = "Biblioteca.txt";
+
+	ifs.open(filename);
+	if (!ifs.is_open()) {
+		cout << "Errore!: file non aperto correttamente.";
+		exit(1);
+	}
+
+	while (!ifs.eof()) {
+		string n, a, p_s;
+		float p;
+
+		getline(ifs, n, ';');
+		getline(ifs, a, ';');
+		getline(ifs, p_s);
+		p = atof(p_s.c_str());
+
+		Libri book(n, a, p);
+
+		v.push_back(book);
+	}
+
+	return v;
+}
+
+
+void salva_libri(vector<Libri> v) {
+	ofstream ofs;
+	string filename = "Biblioteca.txt";
+
+	ofs.open(filename);
+	if (!ofs.is_open()) {
+		cout << "Errore!: file non aperto correttamente.";
+		exit(1);
+	}
+
+	vector<Libri>::iterator it = v.begin();
+	for (it = v.begin(); it != v.end(); it++) {
+		ofs << *it;
+		if (it != v.end() - 1)
+			ofs << endl;
+	}
+}
+
+vector<Clienti> carica_clienti() 
+{
+	vector<Clienti> v;
+
+	ifstream ifs;
+	string filename = "Clienti.txt";
+
+	ifs.open(filename);
+	if (!ifs.is_open()) {
+		cout << "Errore!: file non aperto correttamente.";
+		exit(1);
+	}
+
+	while (!ifs.eof()) {
+		string n, c, cod_fisc, us_name, pass, num, n_prestito;
+		int  num_prestito;
+
+		getline(ifs, n, ';');
+		getline(ifs, c, ';');
+		getline(ifs, cod_fisc, ';');
+		getline(ifs, num, ';');
+		getline(ifs, us_name, ';');
+		getline(ifs, pass, ';');
+		getline(ifs, n_prestito);
+		num_prestito = atoi(n_prestito.c_str());
+		
+		Clienti x(n, c, cod_fisc, us_name, pass, num, num_prestito);
+		v.push_back(x);
+
+	}
+	return v;
+}
+
+void salva_clienti(vector<Clienti>& v)
+{
+	ofstream ofs;
+	string filename = "Clienti.txt";
+
+	ofs.open(filename);
+	if (!ofs.is_open()) {
+		cout << "Errore!: file non aperto correttamente.";
+		exit(1);
+	}
+
+	vector<Clienti>::iterator it;
+	for (it = v.begin(); it != v.end(); ++it) {
+		ofs << (*it).getNome() << ";" << (*it).getCognome() << ";" << (*it).getCod_fiscale() <<
+			";" << (*it).getNumero() << ";" << (*it).getUser() << ";" << (*it).getPassword() << ";"
+			<< (*it).getPrestito();
+		if (it != v.end() - 1)
+			ofs << endl;
+	}
+}
+
+bool login(vector<Clienti> vect, string& u, string& p, string& cf)
+{
+	bool check = false;
+	int i = 6;
+
+	cout << "*********LOGIN*********" << endl;
+	do {
+		cout << "Username: ";
+		getline(cin, u);
+
+		if (u.empty())
+			cout << "ERRORE!\nInserire Username" << endl;
+		if (!is_on(u, vect))
+			cout << "ERRORE!\nUsername non valido" << endl;
+
+	} while (u.empty() || !is_on(u, vect));
+
+	do {
+		cout << "Password: ";
+		getline(cin, p);
+
+		if (p.empty())
+			cout << "ERRORE!\nInserire password" << endl;
+		else if (!is_on(p, vect)) {
+			i--;
+			cout << "ERRORE!\nPassword non valida " << i << "tentativi rimasti" << endl;
+		}
+		else if (i <= 0) {
+			check = false;
+			cout << "Login annullato" << endl;
+			exit(1);
+		}
+		else
+			check = true;
+
+	} while (p.empty() || !is_on(p, vect));
+
+	vector<Clienti>::iterator it;
+	for (it = vect.begin(); it != vect.end(); it++) {
+		if ((*it).getPassword() == p)
+			cf = (*it).getCod_fiscale();
+	}
+
+	return check;
+}
---
+++ InOut/IO.h
@@ -0,0 +1,27 @@
+/// <seealso cref="Biblioteca.h"/>
+#include "Biblioteca.h"
+/// <seealso cref="Libri.h"/>
+#include "Libri.h"
+/// <seealso cref="Clienti.h"/>
+#include"Clienti.h"
+
+/// <param name="DIPUSER">Stringa utilizzata per gestire l'username del dipendente.</param>
+string const DIPUSER = "DIPENDENTE";
+/// <param name="nome">Stringa utilizzata per gestire la password del dipendente.</param>
+string const DIPPASS = "DIPENDENTE123";
+
+/// <item>Funzione di caricamento dei libri nel vettore.</item> 
+vector<Libri> carica_libri();
+
+/// <item>Funzione che integra le modifiche effettuate nel database del programma.</item>
+void salva_libri(vector<Libri> v);
+
+/// <item>Funzione per il caricamento da file di tutti i clienti registrati nel sistema.</item>
+vector<Clienti> carica_clienti();
+
+/// <item>Funzione di salvataggio dei clienti sul file "Clienti.txt".</item>
+void salva_clienti(vector<Clienti> &v);
+
+/// <item>Funzione login per i clienti all'interno del sistema.</item>
+bool login(vector<Clienti> vect, string& u, string& p, string& cf);
+
